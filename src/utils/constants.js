b9e6948e2d77b6cf0a350013b0a55ee4ba39fa5c
anubhav-pratap <pratapanubhavsingh@gmail.com>
2025-08-22 21:46:02 +0530

    fix bugs

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,4 @@
 const BASE_URL =
-  location.hostname === "localhost" ? "http://localhost:7777" : "https://dev-tinder-seven-snowy.vercel.app";
+  location.hostname === "localhost" ? "http://localhost:7777" : "https://devtinder-0j05.onrender.com";
 
 export default BASE_URL;
